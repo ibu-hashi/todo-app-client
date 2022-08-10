3658118e343ff8880cd3e2aa3959ccf5e1e21089
hashino <hashino.ibuki@fujitsu.com>
2022-08-10 14:46:44 +0900

    [add]cssの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import React from 'react';
+import styled from 'styled-components'
+
+const Div = styled.div`
+    color: red;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Div>test</Div>
 }
 
-export default App;
+export default App
